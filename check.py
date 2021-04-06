3c5cbcf93c987310f32e9db5370e25aa261aa985
Ankit Jha <ankitkrjha@gmail.com>
2021-04-06 12:38:07 -0700

    firts commit

---
+++ check.py
@@ -0,0 +1,16 @@
+posts=[
+    {
+        'author':'ankt jha',
+        'title': ' number 1',
+        'content': ' first post conetnt',
+        'date posted': 'April 20 '
+    },
+
+    {
+        'author':'ankt kumar',
+        'title': ' number 2',
+        'content': ' second post content',
+        'date posted': ' April 12 ',
+    }
+]
+for post in posts:
